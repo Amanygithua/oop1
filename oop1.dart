e31b35729f32fe483027bfa8e775fdeaee6fc200
Amanygithua <amanyfysl54@gmail.com>
2022-12-10 15:24:36 +0300

    oop1

---
+++ oop1.dart
@@ -0,0 +1,11 @@
+void main(List<String> args) {
+  user userone=new user();
+  print(userone.username='asss');
+  print(userone.pasw='667');
+}
+class user{
+  String username='';
+  String pasw='';
+  login(){}
+  loguot(){}
+}
